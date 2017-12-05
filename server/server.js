b631119c8c372d289c14829549ef01039732dcad
Mihai Blebea <mihaiserban.blebea@gmail.com>
2017-12-05 15:31:55 +0000

    fix

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const requests = require('./requests.js');
+// const requests = require('./requests.js');
 
 var app = express();
 
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 // POST create new todo
 app.get('/', function(request, response) {
-    requests.createOrder();
+    // requests.createOrder();
     console.log(process.env.SHOPIFY_SHOP_NAME);
     response.send('Merge');
 
