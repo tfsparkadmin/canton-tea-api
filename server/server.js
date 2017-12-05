4aa71276391528a10495dd49ade9c11237089274
Mihai Blebea <mihaiserban.blebea@gmail.com>
2017-12-05 16:34:54 +0000

    fix

--- server/server.js
+++ server/server.js
@@ -23,14 +23,15 @@ app.get('/credit-order', function(request, response) {
 });
 
 app.post('/credit-order', function(request, response) {
-    // axios.headers.post['Content-Type'] = 'application/json';
-    axios.post(api.url, api.payload, {headers: {
-                "Content-Type": "application/json"}
-            }).then((result)=> {
-        response.send('Order placed. Order name is ' + result.data.order.name);
-    }).catch((err)=> {
-        response.send('Nu merge ' + err);
-    });
+
+    response.json(request);
+    // axios.post(api.url, api.payload, {headers: {
+    //             "Content-Type": "application/json"}
+    //         }).then((result)=> {
+    //     response.send('Order placed. Order name is ' + result.data.order.name);
+    // }).catch((err)=> {
+    //     response.send('Nu merge ' + err);
+    // });
 
 });
 
