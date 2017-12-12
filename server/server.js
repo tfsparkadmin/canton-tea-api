e8c5b433974ae70369df491027ba51640325029c
Mihai Blebea <mihaiserban.blebea@gmail.com>
2017-12-12 10:56:40 +0000

    add some cors middleware

--- server/server.js
+++ server/server.js
@@ -9,6 +9,24 @@ app.set('port', (process.env.PORT || 3000));
 // Setup middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://canton-tea.myshopify.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // POST create new todo
 app.get('/credit-order', function(request, response) {
