07bb785c33912828f4bd5f233bbdc2866596096f
Mihai Blebea <mihaiserban.blebea@gmail.com>
2018-01-04 15:02:18 +0000

    fix small test

--- server/server.js
+++ server/server.js
@@ -136,9 +136,9 @@ app.post('/credit-order', function(request, response) {
 
 app.post('/shipping-methods', function(request, response) {
     let payload = request.body;
-    let address = payload.customer.default_address;
+    let address = payload.address;
     let cart    = payload.cart;
-
+    response.send(JSON.stringify(payload));
     let countryCode = address.country_code;
 
     shopify.shippingZone.list({ limit: 5 }).then((zones)=> {
