ef5b687a4b6e29066dcb8466fd55467e0f4b4c8c
Mihai Blebea <mblebea@tfspark.com>
2018-08-15 16:32:26 +0100

    get the payload of the webhook and create the token from it

--- controllers/webhooks.js
+++ controllers/webhooks.js
@@ -4,25 +4,24 @@ const { storeNewUser } = require('./../src/auth')
 const router = express.Router()
 
 router.post('/customer-created', function(request, response) {
-    console.log(request.body)
-    // Must get first name, last name, email and id of the newly creted user
-    // storeNewUser({
-    //     firstName: 'Mihai',
-    //     lastName: 'Blebea',
-    //     email: 'mblebea@tfspark.com'
-    // }, (result)=> {
-    //     storeMetafield({
-    //         key: 'token',
-    //         value: result.token,
-    //         value_type: 'string',
-    //         namespace: 'auth_token',
-    //         owner_resource: 'customer',
-    //         owner_id: customer_id
-    //     }, ()=> {
-    //         response.status(200).json(result.token)
-    //     })
-    // })
-    response.status(200).send()
+    let body = request.body
+    storeNewUser({
+        firstName: body.first_name,
+        lastName: body.last_name,
+        email: body.email,
+        id: body.id
+    }, (result)=> {
+        storeMetafield({
+            key: 'token',
+            value: result.token,
+            value_type: 'string',
+            namespace: 'auth_token',
+            owner_resource: 'customer',
+            owner_id: body.id
+        }, ()=> {
+            response.status(200).json(result.token)
+        })
+    })
 })
 
 
