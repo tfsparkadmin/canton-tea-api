2f52709e98086f07a8749632744ce601320f9eca
Mihai Blebea <mblebea@tfspark.com>
2018-08-24 12:34:24 +0100

    fix env keys

--- src/shopify.js
+++ src/shopify.js
@@ -2,9 +2,9 @@ const Shopify = require('shopify-api-node')
 
 
 const shopify = new Shopify({
-    shopName: process.env.SHOP_NAME,
-    apiKey:   process.env.API_KEY,
-    password: process.env.PASSWORD
+    shopName: process.env.SHOPIFY_SHOP_NAME,
+    apiKey:   process.env.SHOPIFY_API_KEY,
+    password: process.env.SHOPIFY_PASSWORD
 })
 
 module.exports = shopify
